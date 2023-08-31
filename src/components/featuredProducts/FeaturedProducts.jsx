a8ebe85c952a7b1c397447d7ffb760eaa2ac4362
brysonwaisi <brysonnyamwange@gmail.com>
2023-08-31 13:03:47 +0300

    Update changes

--- src/components/featuredProducts/FeaturedProducts.jsx
+++ src/components/featuredProducts/FeaturedProducts.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const FeaturedProducts = () => {
-  return <div>FeaturedProducts</div>;
+  return (
+    <div className="featuredProducts">
+      <div className="top">
+        <h1> products</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan
+          lacus vel facilisis labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida. Risus commodo viverra maecenas.
+        </p>
+      </div>
+      <div className="bottom"></div>
+    </div>
+  );
 };
 
 export default FeaturedProducts;
